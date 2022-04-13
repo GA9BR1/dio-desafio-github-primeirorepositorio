f9697d65e29a23fc60b7720b08822b48a76cbffc
GA9BR1 <gustavoalberttodev@gmail.com>
2022-04-13 11:07:24 -0300

    Adição do diretório aula prática e arquivos da aula

---
+++ Primeiros passos para desenvolvimento web/Aula prática/index.php
@@ -0,0 +1,15 @@
+<html>
+    <head>
+        <title>Meu primeiro site em PHP! Woohoo!</title>
+    </head>
+
+    <body>
+        <?php
+            for ($i = 0; $i < 10; $i++){
+                print("Linha número " . $i . "<br />");            
+            }
+        ?>
+        <a href="index2.php">2 Página</a>
+    </body>
+
+</html>
